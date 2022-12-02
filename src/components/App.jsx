e63cf452ef6fbb8d3007047af432a760eced86e6
Yaroslava-Vorobets <slavavor85@gmail.com>
2022-12-02 12:22:29 +0100

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,46 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import 'modern-normalize';
+
+export class App extends Component {
+  state = {
+  contacts: [],
+  name: ''
+  }
+  
+  hendlerNameCange = e => {
+    this.setState({name: e.currentTarget.value})
+  }
+
+   hendlerContactCange = e => {
+    this.setState({contacts: e.currentTarget.value})
+  }
+  render() {
+    return (
+      <>
+         <h1> Phonebook </h1>
+        <form> 
+          <label  htmlFor="fname">Name</label>
+        <input
+            onChange={this.hendlerNameCange}
+            value={this.state.name}
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          /> 
+          <button>add contact</button>
+      </form>     
+        <h2> Contacts </h2>
+        <ul>
+          <li></li>          
+        </ul>
+       </>
+       
+
+       
+)}
+   
+  }
+
+
